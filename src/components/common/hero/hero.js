110af46646ba4263c3ddb4271947544df26c5a1b
krystek77 <krystian.wrona77@gmail.com>
2020-01-18 22:49:12 +0100

    Create a hero component
    I create a basic structure of hero component

---
+++ src/components/common/hero/hero.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './hero.module.scss';
+import bgDefault from '../../../assets/images/hero/bg_default.png';
+
+function Hero({ heroClass, img, alt, label, title, description, children }) {
+  return (
+    <header className={`${styles.hero} ${styles[heroClass]}`}>
+      <img className={styles.hero__background} src={img} alt={alt} />
+      <div className={styles.hero__banner}>
+        <p className={styles.hero__label}>{label}</p>
+        <h1 className={styles.hero__title}>{title}</h1>
+        <p className={styles.hero__description}>{description}</p>
+      </div>
+      {children}
+    </header>
+  );
+}
+
+Hero.propTypes = {
+  heroClass: PropTypes.arrayOf(PropTypes.string),
+  img: PropTypes.string,
+  alt: PropTypes.string,
+  label: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  children: PropTypes.node,
+};
+
+Hero.defaultProps = {
+  heroClass: ['hero-default'],
+  img: bgDefault,
+  alt: 'Default image description',
+  label: 'Page label',
+  title: 'Page title',
+  description: 'Default page description',
+  children: null,
+};
+export default Hero;
