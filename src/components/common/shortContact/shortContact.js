25b499e2d27041ebc74db11e50bff47a3c791a36
krystek77 <krystian.wrona77@gmail.com>
2020-01-21 09:01:15 +0100

    reate template of short contact component

---
+++ src/components/common/shortContact/shortContact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './shortContact.module.scss';
+
+function ShortContact() {
+  return (
+    <div className={styles['short-contact']}>
+      <div className={styles.container}> --- short contact --- </div>
+    </div>
+  );
+}
+
+export default ShortContact;
