aaf2d2d4aafb80eb48d0a155b3c4898dffbdcd9a
krystek77 <krystian.wrona77@gmail.com>
2020-01-21 08:57:17 +0100

    Create SEO component

---
+++ src/components/common/seo/seo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+function SEO() {
+  return (
+    <Helmet>
+      <meta charSet='utf-8' />
+      <meta name='description' content='' />
+      <meta name='keywords' content='' />
+      <meta name='author' content='Krystian Wrona' />
+      <title>
+        Przedsiębiorstwo Produkcji Urządzeń Pralniczych Pralma sp. z o.o.
+      </title>
+    </Helmet>
+  );
+}
+
+export default SEO;
