a043a12d96c4085f2eeddc656bd6386b36468d67
krystek77 <krystian.wrona77@gmail.com>
2020-01-21 11:21:08 +0100

    Refactor navigation component
    I replaced a lot of code with new main menu component

--- src/components/common/menu/navigation/navigation.js
+++ src/components/common/menu/navigation/navigation.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { NavLink as Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './navigation.module.scss';
 
 import SigninMenu from './signinMenu';
+import MainMenu from './mainMenu';
 
 function Navigation({ isOpen }) {
   const navigationClass = isOpen
@@ -12,36 +12,8 @@ function Navigation({ isOpen }) {
 
   return (
     <div className={navigationClass}>
-      {/** mainMenu */}
       <div className={styles.inner}>
-        <ul className={styles['main-menu']}>
-          <li className={styles['main-menu__item']}>
-            <Link
-              to='/'
-              className={styles['main-menu__link']}
-              exact
-              activeClassName={styles.active}>
-              Home
-            </Link>
-          </li>
-          <li className={styles['main-menu__item']}>
-            <Link
-              to='/produkty'
-              className={styles['main-menu__link']}
-              activeClassName={styles.active}>
-              Produkty
-            </Link>
-          </li>
-          <li className={styles['main-menu__item']}>
-            <Link
-              to='/technologie'
-              className={styles['main-menu__link']}
-              activeClassName={styles.active}>
-              Technologie
-            </Link>
-          </li>
-        </ul>
-        {/** signinMenu */}
+        <MainMenu />
         <SigninMenu />
         {/** signoutMenu */}
       </div>
