02dfe9a3e8dca2ccc6ffd3ef7a091e5f0f6a4e31
krystek77 <krystian.wrona77@gmail.com>
2020-01-23 10:14:32 +0100

    Create picture component
    Image has position absolute to it container and center position

---
+++ src/components/common/picture/picture.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './picture.module.scss';
+import defaultImage from '../../../assets/images/default_460_610.png';
+
+function Picture({ src, alt, width }) {
+  return (
+    <div className={styles['image-wrapper']} style={{ width: `${width}px` }}>
+      <img className={styles['image-wrapper__image']} src={src} alt={alt} />
+    </div>
+  );
+}
+Picture.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  width: PropTypes.number,
+};
+Picture.defaultProps = {
+  src: defaultImage,
+  alt: 'Default description of image',
+  width: 320,
+};
+
+export default Picture;
