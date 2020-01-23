ce6e9933eda3ef101b37b729f80bd7bacdb97b23
krystek77 <krystian.wrona77@gmail.com>
2020-01-23 10:45:05 +0100

    Create basic structure of consumption component

---
+++ src/components/home/whyWe/consumption/consumption.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './consumption.module.scss';
+
+function Consumption() {
+  return (
+    <article className={styles.consumption}>
+      <p> --- I am consumption tab ---</p>
+    </article>
+  );
+}
+
+export default Consumption;
