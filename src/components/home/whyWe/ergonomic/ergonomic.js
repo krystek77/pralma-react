772c13e6e44910d20a029522985f16d34dc37100
krystek77 <krystian.wrona77@gmail.com>
2020-01-23 10:47:03 +0100

    Create basic structure of ergonomic component

---
+++ src/components/home/whyWe/ergonomic/ergonomic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ergonomic.module.scss';
+
+function Ergonomic() {
+  return (
+    <article className={styles.ergonomic}>
+      <p> --- I am sleek and ergonomic design tab ---</p>
+    </article>
+  );
+}
+
+export default Ergonomic;
