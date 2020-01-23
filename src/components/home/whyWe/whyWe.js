237b0d0de4efcf622fed12e7e871ed3c74127885
krystek77 <krystian.wrona77@gmail.com>
2020-01-23 08:01:07 +0100

    Remove deleted content component

--- src/components/home/whyWe/whyWe.js
+++ src/components/home/whyWe/whyWe.js
@@ -4,7 +4,6 @@ import styles from './whyWe.module.scss';
 import Title from '../../common/title';
 import Tabs from '../../common/tabs';
 import Tab from '../../common/tabs/tab';
-import ContentTab from './content';
 
 function WhyWe() {
   return (
@@ -13,16 +12,16 @@ function WhyWe() {
         <Title title='Dlaczego Pralma' />
         <Tabs>
           <Tab titleTab='Niskie koszty'>
-            <ContentTab />
-          </Tab>
-          <Tab titleTab='Wysoka wydajność'>
             <div>--- tab content 1 ---</div>
           </Tab>
-          <Tab titleTab='Elegancja i ergonomia'>
+          <Tab titleTab='Wysoka wydajność'>
             <div>--- tab content 2 ---</div>
           </Tab>
-          <Tab titleTab='Przyjazne dla środowiska'>
+          <Tab titleTab='Elegancja i ergonomia'>
             <div>--- tab content 3 ---</div>
+          </Tab>
+          <Tab titleTab='Przyjazne dla środowiska'>
+            <div>--- tab content 4 ---</div>
           </Tab>
         </Tabs>
       </div>
