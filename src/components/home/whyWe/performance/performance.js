82514c29045f692ea86b145dcbe5dafe272d56d6
krystek77 <krystian.wrona77@gmail.com>
2020-01-23 10:46:25 +0100

    Create basic structure of performance component

---
+++ src/components/home/whyWe/performance/performance.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './performance.module.scss';
+
+function Performance() {
+  return (
+    <article className={styles.performance}>
+      <p> --- I am performance tab ---</p>
+    </article>
+  );
+}
+
+export default Performance;
