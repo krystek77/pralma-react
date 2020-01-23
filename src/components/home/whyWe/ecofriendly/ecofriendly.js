cbd55a168655fba53f24a4ca4156b89a0054d917
krystek77 <krystian.wrona77@gmail.com>
2020-01-23 10:45:50 +0100

    Create basic structure of ecofriendly component

---
+++ src/components/home/whyWe/ecofriendly/ecofriendly.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ecofriendly.module.scss';
+
+function EcoFriendly() {
+  return (
+    <article className={styles['eco-friendly']}>
+      <p> --- I am eco-friendly focused tab ---</p>
+    </article>
+  );
+}
+
+export default EcoFriendly;
