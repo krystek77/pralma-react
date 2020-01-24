69bfadaae254618584ccc0a10e198c6a9f436878
krystek77 <krystian.wrona77@gmail.com>
2020-01-24 13:08:09 +0100

    Modify button link component
    I added fullMenu props to adjust button link to match the look of it in the full menu

--- src/components/common/buttonLink/buttonLink.js
+++ src/components/common/buttonLink/buttonLink.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink as Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './buttonLink.module.scss';
 
-function ButtonLink({ path, label, children, active, disabled }) {
+function ButtonLink({ path, label, children, disabled, fullMenu }) {
   let buttonClass = '';
-  /**
-   * if present active must be active
-   * active -> active link
-   * disabled -> disabled link
-   * active disabled -> active
-   * disabled active -> active
-   *
-   */
-  if (active) {
-    buttonClass = `${styles.btn} ${styles['btn-active']}`;
-  } else if (disabled) {
-    buttonClass = `${styles.btn} ${styles['btn-disabled']}`;
-  } else {
-    buttonClass = `${styles.btn}`;
-  }
+
+  buttonClass = disabled
+    ? `${styles.btn} ${styles['btn-disabled']}`
+    : `${styles.btn}`;
+
+  // `${styles['red']} ${styles['green']}`
+
+  buttonClass = fullMenu
+    ? buttonClass
+        .split(' ')
+        .concat(`${styles['btn-fullmenu']}`)
+        .join(' ')
+    : buttonClass;
+
   return (
-    <Link to={path} className={buttonClass}>
+    <Link
+      to={path}
+      className={buttonClass}
+      activeClassName={styles['btn-active']}>
       <span className={styles.btn__icon}>{children}</span>
       <span className={styles.btn__label}>{label}</span>
     </Link>
@@ -32,14 +34,14 @@ ButtonLink.propTypes = {
   path: PropTypes.string,
   label: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.object]),
-  active: PropTypes.bool,
   disabled: PropTypes.bool,
+  fullMenu: PropTypes.bool,
 };
 
 ButtonLink.defaultProps = {
   path: '/a',
   children: null,
-  active: false,
   disabled: false,
+  fullMenu: false,
 };
 export default ButtonLink;
