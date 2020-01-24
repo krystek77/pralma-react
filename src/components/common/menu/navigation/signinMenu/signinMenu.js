4b339629dcc17a6abcbbe70aad475396d956d7f9
krystek77 <krystian.wrona77@gmail.com>
2020-01-24 13:17:17 +0100

    Modify signin menu using button link component

--- src/components/common/menu/navigation/signinMenu/signinMenu.js
+++ src/components/common/menu/navigation/signinMenu/signinMenu.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { GoSignIn } from 'react-icons/go';
 import { FaRegUser } from 'react-icons/fa';
 import styles from './signinMenu.module.scss';
+import ButtonLink from '../../../buttonLink';
 
 function SigninMenu() {
   return (
     <ul className={styles.signinMenu}>
       <li className={styles.signinMenu__item}>
-        <Link to='/signin' className={styles.signinMenu__link}>
-          <GoSignIn className={styles.signinMenu__icon} />
-          <span className={styles.signinMenu__label}>Zaloguj się</span>
-        </Link>
+        <ButtonLink path='/logowanie' label='zaloguj' fullMenu>
+          <GoSignIn />
+        </ButtonLink>
       </li>
       <li className={styles.signinMenu__item}>
-        <Link to='/signup' className={styles.signinMenu__link}>
-          <FaRegUser className={styles.signinMenu__icon} />
-          <span className={styles.signinMenu__label}>Zarejestruj się</span>
-        </Link>
+        <ButtonLink path='/rejestracja' label='zarejestruj' fullMenu>
+          <FaRegUser />
+        </ButtonLink>
       </li>
     </ul>
   );
