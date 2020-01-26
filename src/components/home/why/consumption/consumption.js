9a09cf987e9edd792fa2ddb8ef2be4c912f1461a
krystek77 <krystian.wrona77@gmail.com>
2020-01-26 19:37:15 +0100

    Build new structure of consumption component
    Added necessery styles too.

--- src/components/home/why/consumption/consumption.js
+++ src/components/home/why/consumption/consumption.js
@@ -10,52 +10,65 @@ import ButtonLink from '../../../common/buttonLink';
 
 function Consumption() {
   return (
-    <article className={styles.consumption}>
-      <div className={styles.conatiner}>
-        <Title text='Optymalizacja kosztów' level='lvl-3' />
+    <article className={styles.consumptionm}>
+      <div className={styles.container}>
+        {/** start of container */}
+        {/** header */}
+        <header className={styles.header}>
+          <Title text='Optymalizacja kosztów' level='lvl-3' />
+          <p className={styles.header__description}>
+            Water and energy use are a huge concerns for every stakeholder and
+            client in the industry. With this in mind, the FX Line has been
+            designed to achieve extremely low water and energy consumption. FX
+            washer-extractors are standard equipped with revolutionary CASCADE
+            TM DRUM
+          </p>
+        </header>
+        {/** main */}
+        <div className={styles.main}>
+          <Title
+            text='PRIMUS introduces the unique CASCADE TM DRUM'
+            level='lvl-4'
+          />
+          <div className={styles.content}>
+            <aside className={styles.content__images}>
+              <Picture alt='Cascade drum' width={320} />
+              <Picture alt='Cascade drum' width={320} />
+            </aside>
+            <div className={styles.content__description}>
+              <p className={styles.content__text}>
+                The CASCADE DRUM concept results in more perforations in the
+                drum over the same surface area – so water extraction is much
+                more efficient without your linen sticking to the drum, thus
+                simplifying unloading. The CASCADE shape of the drum holes
+                provides an extra mechanical action that produces an even better
+                washing result
+              </p>
+              <div className={styles.content__list}>
+                <UnorderedList>
+                  <ListItem text='Simpler unloading, as your linen does not stick to the drum' />
+                  <ListItem text='Maximum water extraction, thanks to more perforations in the drum over the same surface' />
+                  <ListItem text='Less residual moisture, due to the inclination of the bulges that extracts even more water.' />
+                  <ListItem text='Better washing performance, achieved by the specific shape of the bulges that provide an extra mechanical action' />
+                </UnorderedList>
+              </div>
 
-        <p className={styles.description}>
-          Water and energy use are a huge concerns for every stakeholder and
-          client in the industry. With this in mind, the FX Line has been
-          designed to achieve extremely low water and energy consumption. FX
-          washer-extractors are standard equipped with revolutionary CASCADE TM
-          DRUM
-        </p>
-        <div className={styles.grid}>
-          <Picture alt='Cascade drum' width={320} />
-
-          <div className={styles['group-items']}>
-            <Profit value={25} label='Mniejsza wigotność resztkowa' />
-            <Profit
-              value={20}
-              label='Mniejsze zużycie energii w procesie suszenia'
-            />
+              <div className={styles.content__profits}>
+                <Profit value={25} label='Mniejsza wigotność resztkowa' />
+                <Profit
+                  value={20}
+                  label='Mniejsze zużycie energii w procesie suszenia'
+                />
+                <Profit value={15} label='Mniejsze zużycie wody' />
+              </div>
+            </div>
           </div>
         </div>
-
-        <Title
-          text='PRIMUS introduces the unique CASCADE TM DRUM'
-          level='lvl-4'
-        />
-        <Picture alt='Cascade drum' width={320} />
-        <p className={styles.description}>
-          The CASCADE DRUM concept results in more perforations in the drum over
-          the same surface area – so water extraction is much more efficient
-          without your linen sticking to the drum, thus simplifying unloading.
-          The CASCADE shape of the drum holes provides an extra mechanical
-          action that produces an even better washing result
-        </p>
-
-        <UnorderedList>
-          <ListItem text='Simpler unloading, as your linen does not stick to the drum' />
-          <ListItem text='Maximum water extraction, thanks to more perforations in the drum over the same surface' />
-          <ListItem text='Less residual moisture, due to the inclination of the bulges that extracts even more water.' />
-          <ListItem text='Better washing performance, achieved by the specific shape of the bulges that provide an extra mechanical action' />
-        </UnorderedList>
-
-        <Profit value={15} label='Mniejsze zużycie wody' />
-
-        <ButtonLink path='/produkty' label='Zobacz produkty' />
+        {/** footer */}
+        <footer className={styles.footer}>
+          <ButtonLink path='/produkty' label='Zobacz produkty' />
+        </footer>
+        {/** end of container */}
       </div>
     </article>
   );
