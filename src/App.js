886b5348bc0b195247c47573eaba47599f21e7e1
krystek77 <krystian.wrona77@gmail.com>
2020-01-28 07:34:27 +0100

    Import modal component to App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Modal from './components/common/modal';
 import HomePage from './components/pages/home';
 import ProductsPage from './components/pages/products';
 import TechnologiesPage from './components/pages/technologies';
@@ -9,13 +10,16 @@ import ErrorPage from './components/pages/404';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/produkty' component={ProductsPage} />
-      <Route exact path='/technologie' component={TechnologiesPage} />
-      <Route exact path='/logowanie' component={SigninPage} />
-      <Route component={ErrorPage} />
-    </Switch>
+    <>
+      <Modal />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/produkty' component={ProductsPage} />
+        <Route exact path='/technologie' component={TechnologiesPage} />
+        <Route exact path='/logowanie' component={SigninPage} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </>
   );
 }
 
