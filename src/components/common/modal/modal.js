dccab3045250565b8e9890aedb91e49ff5fa3961
krystek77 <krystian.wrona77@gmail.com>
2020-01-28 11:32:11 +0100

    Add properties handleAccept and prop types

--- src/components/common/modal/modal.js
+++ src/components/common/modal/modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { IoIosCloseCircle } from 'react-icons/io';
 import styles from './modal.module.scss';
 
@@ -7,13 +8,12 @@ import Button from '../button';
 import UnorderedList from '../unorderedList';
 import ListItem from '../unorderedList/listItem';
 
-
-function Modal() {
+function Modal({ handleAccept }) {
   return (
     <div className={styles['backround-overlay']}>
       <div className={styles.modal}>
         <div className={styles.modal__topbar}>
-          <Logo logoClass="logo-modal" />
+          <Logo logoClass='logo-modal' />
           <IoIosCloseCircle className={styles.modal__close} />
         </div>
         <div className={styles.modal__content}>
@@ -49,25 +49,13 @@ function Modal() {
               danych osobowych na terenie UE)
             </p>
             <UnorderedList>
-              <ListItem
-                text="Administratorem Pani/Pana danych osobowych jest: Przedsiębiorstwo Produkcji Urządzeń Pralniczych “Pralma” Sp. z o.o. ul. Górnicza 49a, 25-651 Kielce NIP 6570081116, KRS 0000022564, tel. 41/3457408, 41/3450561, biuro@pralma.pl."
-              />
-              <ListItem
-                text="Administrator nie wyznaczył Inspektora Ochrony Danych Osobowych."
-              />
-              <ListItem
-                text="Ma Pani/Pan prawo dostępu do swoich danych, otrzymania ich kopii, ich sprostowania, usunięcia, ograniczenia przetwarzania, oraz prawo do przenoszenia danych, a także wniesienia sprzeciwu."
-              />
-              <ListItem
-                text="Do Pani/Pana danych mogą mieć dostęp podmioty przetwarzające (działające na nasze zlecenie), takie jak: firmy księgowe, kancelarie prawnicze, firmy kurierskie, podmioty świadczące usługi płatności elektronicznych, administrator systemu sprzedażowego, dostawcy usług IT. Podanie danych jest dobrowolne, ale niezbędne do realizacji powyższych celów, w szczególności obowiązków prawnych Spółki, w tym realizacji Państwa zgłoszeń lub spraw (bez ich podania nasza Spółka nie będzie mogła realizować swych obowiązków, w tym Państwa zgłoszenia)"
-              />
-              <ListItem text="Dane przechowywane są bezterminowo." />
-              <ListItem
-                text="Przysługuje Pani/Panu wniesienia skargi do organu nadzorczego (Prezesa Urzędu Ochrony Danych Osobowych), jeśli Pani/Pana zdaniem przetwarzanie danych osobowych Pani/Pana narusza przepisy rozporządzenia RODO."
-              />
-              <ListItem
-                text="Pani/Pana dane osobowe nie będą przekazywane do państw trzecich i organizacji międzynarodowych. Informujemy, że firma P. P.U.P. „Pralma” Sp. z o. o. dokłada wszelkich starań, aby zapewnić wszelkie środki fizycznej, technicznej i organizacyjnej ochrony danych osobowych przed ich przypadkowym bądź umyślnym zniszczeniem, przypadkową utratą, nieuprawnionym ujawnieniem lub wykorzystaniem, zgodnie z wszelkimi obowiązującym przepisami."
-              />
+              <ListItem text='Administratorem Pani/Pana danych osobowych jest: Przedsiębiorstwo Produkcji Urządzeń Pralniczych “Pralma” Sp. z o.o. ul. Górnicza 49a, 25-651 Kielce NIP 6570081116, KRS 0000022564, tel. 41/3457408, 41/3450561, biuro@pralma.pl.' />
+              <ListItem text='Administrator nie wyznaczył Inspektora Ochrony Danych Osobowych.' />
+              <ListItem text='Ma Pani/Pan prawo dostępu do swoich danych, otrzymania ich kopii, ich sprostowania, usunięcia, ograniczenia przetwarzania, oraz prawo do przenoszenia danych, a także wniesienia sprzeciwu.' />
+              <ListItem text='Do Pani/Pana danych mogą mieć dostęp podmioty przetwarzające (działające na nasze zlecenie), takie jak: firmy księgowe, kancelarie prawnicze, firmy kurierskie, podmioty świadczące usługi płatności elektronicznych, administrator systemu sprzedażowego, dostawcy usług IT. Podanie danych jest dobrowolne, ale niezbędne do realizacji powyższych celów, w szczególności obowiązków prawnych Spółki, w tym realizacji Państwa zgłoszeń lub spraw (bez ich podania nasza Spółka nie będzie mogła realizować swych obowiązków, w tym Państwa zgłoszenia)' />
+              <ListItem text='Dane przechowywane są bezterminowo.' />
+              <ListItem text='Przysługuje Pani/Panu wniesienia skargi do organu nadzorczego (Prezesa Urzędu Ochrony Danych Osobowych), jeśli Pani/Pana zdaniem przetwarzanie danych osobowych Pani/Pana narusza przepisy rozporządzenia RODO.' />
+              <ListItem text='Pani/Pana dane osobowe nie będą przekazywane do państw trzecich i organizacji międzynarodowych. Informujemy, że firma P. P.U.P. „Pralma” Sp. z o. o. dokłada wszelkich starań, aby zapewnić wszelkie środki fizycznej, technicznej i organizacyjnej ochrony danych osobowych przed ich przypadkowym bądź umyślnym zniszczeniem, przypadkową utratą, nieuprawnionym ujawnieniem lub wykorzystaniem, zgodnie z wszelkimi obowiązującym przepisami.' />
             </UnorderedList>
             <p className={styles.modal__paragraph}>
               <strong>Cookies</strong>
@@ -92,12 +80,19 @@ function Modal() {
           </div>
         </div>
         <div className={styles.modal__actions}>
-          <Button type="button" label="odrzuć" />
-          <Button type="button" label="akceptuj" />
+          <Button type='button' label='odrzuć' />
+          <Button
+            type='button'
+            label='akceptuj'
+            handleClickButton={handleAccept}
+          />
         </div>
       </div>
     </div>
   );
 }
 
+Modal.propTypes = {
+  handleAccept: PropTypes.func.isRequired,
+};
 export default Modal;
