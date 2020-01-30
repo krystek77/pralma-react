d2df2d96726752a154896486b765b6c5884d77e3
krystek77 <krystian.wrona77@gmail.com>
2020-01-30 16:37:54 +0100

    Import technology component to home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,6 +3,7 @@ import Layout from '../common/layout';
 import Hero from '../common/hero';
 import ServiceSection from '../home/service';
 import WhySection from '../home/why';
+import Technology from '../home/technology';
 import bgImage from '../../assets/images/hero/bg_home.png';
 
 function HomePage() {
@@ -19,6 +20,7 @@ function HomePage() {
           integrated quality vectors. Authoritatively parallel task low-risk
           high-yield e-tailers for real-time niche.'
       />
+      <Technology />
       <WhySection />
       <ServiceSection />
     </Layout>
