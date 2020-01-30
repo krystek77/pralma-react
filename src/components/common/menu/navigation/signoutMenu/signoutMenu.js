7c96e60a584b87d0370aecbf0cda20add02fb6da
krystek77 <krystian.wrona77@gmail.com>
2020-01-30 10:17:32 +0100

    Add greeting component to signout menu

--- src/components/common/menu/navigation/signoutMenu/signoutMenu.js
+++ src/components/common/menu/navigation/signoutMenu/signoutMenu.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { GoSignOut } from 'react-icons/go';
 import styles from './signoutMenu.module.scss';
 
 import Button from '../../../button';
+import Greeting from '../../../greeting';
 
-function SignoutMenu() {
+function SignoutMenu({ isOpen }) {
   return (
-    <ul className={styles.signoutMenu}>
-      <li className={styles.signoutMenu__item}>
-        <Button type="button" label="wyloguj" fullMenu>
-          <GoSignOut />
-        </Button>
-      </li>
-    </ul>
+    <>
+      {isOpen ? null : <Greeting />}
+      <ul className={styles.signoutMenu}>
+        <li className={styles.signoutMenu__item}>
+          <Button type='button' label='wyloguj' fullMenu>
+            <GoSignOut />
+          </Button>
+        </li>
+      </ul>
+    </>
   );
 }
+SignoutMenu.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+};
 export default SignoutMenu;
