0661ad1cff9abfe34699194aba2a047b89142535
krystek77 <krystian.wrona77@gmail.com>
2020-01-31 11:24:20 +0100

    Create technology component

--- src/components/home/technology/technology.js
+++ src/components/home/technology/technology.js
@@ -1,12 +1,70 @@
 import React from 'react';
-import Title from '../../common/title';
 import styles from './technology.module.scss';
+
+import Slides from './slides';
+import Slide from './slides/slide';
+import Title from '../../common/title';
+import Picture from "../../common/picture";
+import defaultTehcnologyImage from '../../../assets/images/technology/technology_thumble_260_160_default.png';
 
 function Technology() {
   return (
     <section className={styles.technology}>
-      <Title level="lvl-2" text="Innowacyjne technologie" />
-      <div>---content ---</div>
+      <div className={styles.container}>
+        <Title level='lvl-2' text='Innowacyjne technologie' />
+        <Slides>
+          <Slide>
+            <Picture src={defaultTehcnologyImage} width={270} />
+            <Title level="lvl-3" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+              error corporis itaque ipsam, fuga voluptate pariatur explicabo
+              doloribus magnam beatae perspiciatis delectus alias expedita sint
+              repudiandae, distinctio sequi unde dolores.
+            </p>
+          </Slide>
+          <Slide>
+            <Picture src={defaultTehcnologyImage} width={270} />
+            <Title level="lvl-3" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+              error corporis itaque ipsam, fuga voluptate pariatur explicabo
+              doloribus magnam beatae perspiciatis delectus alias expedita sint
+              repudiandae, distinctio sequi unde dolores.
+            </p>
+          </Slide>
+          <Slide>
+            <Picture src={defaultTehcnologyImage} width={270} />
+            <Title level="lvl-3" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+              error corporis itaque ipsam, fuga voluptate pariatur explicabo
+              doloribus magnam beatae perspiciatis delectus alias expedita sint
+              repudiandae, distinctio sequi unde dolores.
+            </p>
+          </Slide>
+          <Slide>
+            <Picture src={defaultTehcnologyImage} width={270} />
+            <Title level="lvl-3" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+              error corporis itaque ipsam, fuga voluptate pariatur explicabo
+              doloribus magnam beatae perspiciatis delectus alias expedita sint
+              repudiandae, distinctio sequi unde dolores.
+            </p>
+          </Slide>
+          <Slide>
+            <Picture src={defaultTehcnologyImage} width={270} />
+            <Title level="lvl-3" />
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+              error corporis itaque ipsam, fuga voluptate pariatur explicabo
+              doloribus magnam beatae perspiciatis delectus alias expedita sint
+              repudiandae, distinctio sequi unde dolores.
+            </p>
+          </Slide>
+        </Slides>
+      </div>
     </section>
   );
 }
