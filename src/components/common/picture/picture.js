c1787fe365e04f383255b939023e245c015ae92e
krystek77 <krystian.wrona77@gmail.com>
2020-01-31 11:23:02 +0100

    Change propery width to max width in picture comp

--- src/components/common/picture/picture.js
+++ src/components/common/picture/picture.js
@@ -5,7 +5,7 @@ import defaultImage from '../../../assets/images/default_460_460.png';
 
 function Picture({ src, alt, width }) {
   return (
-    <div className={styles['image-wrapper']} style={{ width: `${width}px` }}>
+    <div className={styles['image-wrapper']} style={{ maxWidth: `${width}px` }}>
       <img className={styles['image-wrapper__image']} src={src} alt={alt} />
     </div>
   );
