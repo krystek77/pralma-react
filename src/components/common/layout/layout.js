4c1d09b5809eb1717be07c1ab54a4ce83782167c
krystek77 <krystian.wrona77@gmail.com>
2020-02-08 11:59:26 +0100

    Remove children label from layout component

--- src/components/common/layout/layout.js
+++ src/components/common/layout/layout.js
@@ -13,7 +13,7 @@ function Layout({ children }) {
       <SEO />
       <Menu />
       <ShortContact />
-      <div className={styles.content}>{children} -- children --</div>
+      <div className={styles.content}>{children}</div>
       <Footer />
     </div>
   );
