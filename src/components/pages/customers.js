53fd956c7d90cbaa38cf30f17c85b83ae88d7154
krystek77 <krystian.wrona77@gmail.com>
2020-02-08 19:16:19 +0100

    Create page content for customers page

--- src/components/pages/customers.js
+++ src/components/pages/customers.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Layout from '../common/layout';
 import Hero from '../common/hero';
 import PageContent from '../common/pageContent';
+import Main from '../common/main';
+import AsideNavigation from '../common/asideNavigation';
+import TitleNavigation from '../common/asideNavigation/titleNavigation';
+import LinksList from '../common/linksList';
+import LinkList from '../common/linksList/linkList';
 
-function CustomersPage(props) {
-  console.log(props);
+function CustomersPage({ match }) {
+  const { url, params } = match;
   return (
     <Layout>
       <Hero
         heroClass={['hero-customers']}
-        alt='Okładka strony klienci'
+        alt='Okładka strony Klienci'
         label='Nasi Klienci'
         title='Przemysłowe wyposażenie pralnicze dla każdego rodzaju pralni'
         description='Globally maximize innovative functionalities after dynamic
@@ -18,20 +24,64 @@ function CustomersPage(props) {
           high-yield e-tailers for real-time niche.'
       />
       <PageContent>
-        <main style={{ backgroundColor: 'grey' }}>--- main customers ---</main>
-        <div style={{ backgroundColor: 'orange' }}>
-          --- navigation customers ---
-        </div>
-        <aside
-          style={{
-            backgroundColor: 'blue',
-            gridColumnStart: 1,
-            gridColumnEnd: 'span 2',
-          }}>
-          --- links customers ---
-        </aside>
+        <Main>{params.slug}</Main>
+        <AsideNavigation>
+          {/** link to customers page */}
+          <TitleNavigation path={`${url}`} label='Nasi Klienci' />
+          {/** links to single customer */}
+          <LinksList>
+            <LinkList path={`${url}/goscinnosc`} label='Gościnność' />
+            <LinkList
+              path={`${url}/opieka-dlugoterminowa`}
+              label='Opieka długoterminowa'
+            />
+            <LinkList
+              path={`${url}/pralnie-komercyjne`}
+              label='Pralnie komercyjne'
+            />
+            <LinkList path={`${url}/straz-pozarna`} label='Straż pożarna' />
+            <LinkList path={`${url}/kluby-sportowe`} label='Kluby sportowe' />
+            <LinkList
+              path={`${url}/pralnie-chemiczne`}
+              label='Pralnie chemiczne'
+            />
+            <LinkList path={`${url}/restauracje`} label='Restauracje' />
+            <LinkList path={`${url}/wellness-spa`} label='Wellness & SPA' />
+            <LinkList path={`${url}/armia`} label='Armia' />
+            <LinkList
+              path={`${url}/marynarka-wojenna`}
+              label='Marynarka Wojenna'
+            />
+            <LinkList
+              path={`${url}/obiekty-mieszkalne`}
+              label='Obiekty mieszkalne'
+            />
+            <LinkList path={`${url}/zaklady-pracy`} label='Zakłady pracy' />
+            <LinkList
+              path={`${url}/pralnie-samoobslugowe`}
+              label='Pralnie samoobsługowe'
+            />
+            <LinkList path={`${url}/szpitale`} label='Szpitale' />
+            <LinkList
+              path={`${url}/opieka-nad-dziecmi`}
+              label='Opieka nad dziećmi'
+            />
+          </LinksList>
+        </AsideNavigation>
       </PageContent>
+      <aside
+        style={{
+          backgroundColor: 'blue',
+        }}>
+        --- links customers ---
+      </aside>
     </Layout>
   );
 }
+CustomersPage.propTypes = {
+  match: PropTypes.shape({
+    url: PropTypes.string,
+    params: PropTypes.object,
+  }).isRequired,
+};
 export default CustomersPage;
