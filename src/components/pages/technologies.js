65d59e687a5b14ef302898bfbc4bd581fab4d348
krystek77 <krystian.wrona77@gmail.com>
2020-02-08 19:27:12 +0100

    Create page content for technologies page

--- src/components/pages/technologies.js
+++ src/components/pages/technologies.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Layout from '../common/layout';
 import Hero from '../common/hero';
+import PageContent from '../common/pageContent';
+import Main from '../common/main';
+import AsideNavigation from '../common/asideNavigation';
+import TitleNavigation from '../common/asideNavigation/titleNavigation';
+import LinksList from '../common/linksList';
+import LinkList from '../common/linksList/linkList';
 import bgImage from '../../assets/images/hero/bg_technologies.png';
 
-function TechnologiesPage() {
+function TechnologiesPage({ match }) {
+  const { url, params } = match;
   return (
     <Layout>
       <Hero
@@ -17,8 +25,38 @@ function TechnologiesPage() {
           integrated quality vectors. Authoritatively parallel task low-risk
           high-yield e-tailers for real-time niche.'
       />
+      <PageContent>
+        <Main>{params.slug}</Main>
+        <AsideNavigation>
+          <TitleNavigation path={`${url}`} label='Technologie' />
+          <LinksList>
+            <LinkList path={`${url}/cascade-drum`} label='Cascade Drum' />
+            <LinkList path={`${url}/dimple-dry`} label='DimpleDry' />
+            <LinkList path={`${url}/ease-soap`} label='easySOAP' />
+            <LinkList path={`${url}/trace-tech`} label='TRACE-TECH' />
+            <LinkList path={`${url}/optiload`} label='OPTILOAD' />
+            <LinkList path={`${url}/power-wash`} label='PowerWash' />
+            <LinkList path={`${url}/evo4`} label='EVO4' />
+            <LinkList path={`${url}/senso-dry`} label='senso.dry' />
+            <LinkList path={`${url}/eco`} label='eco' />
+            <LinkList path={`${url}/radax`} label='radax' />
+            <LinkList path={`${url}/senso-speed`} label='senso.speed' />
+            <LinkList path={`${url}/ultra-balance`} label='ultra-balance' />
+            <LinkList path={`${url}/softwash`} label='softwash' />
+            <LinkList path={`${url}/shower-jet`} label='Shower-Jet' />
+            <LinkList path={`${url}/xcontrol-plus`} label='XControl Plus' />
+          </LinksList>
+        </AsideNavigation>
+      </PageContent>
     </Layout>
   );
 }
+
+TechnologiesPage.propTypes = {
+  match: PropTypes.shape({
+    url: PropTypes.string,
+    params: PropTypes.object,
+  }).isRequired,
+};
 
 export default TechnologiesPage;
