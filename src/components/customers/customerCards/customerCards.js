efd12f49bc9d24d9262bd639bb329fd60d736ead
krystek77 <krystian.wrona77@gmail.com>
2020-02-10 09:47:21 +0100

    Create customer cards component

---
+++ src/components/customers/customerCards/customerCards.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './customerCards.module.scss';
+
+function CustomerCards({ children }) {
+  return <div className={styles['customer-cards']}>{children}</div>;
+}
+
+CustomerCards.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element),
+  ]).isRequired,
+};
+export default CustomerCards;
