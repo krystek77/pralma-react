e158654abb9caf71a2bf35b4c9cfbb1dc7c6d043
krystek77 <krystian.wrona77@gmail.com>
2020-02-10 09:59:48 +0100

    Create links component for customers page

---
+++ src/components/customers/customerLinks/customerLinks.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './customerLinks.module.scss';
+
+function CustomerLinks({ children }) {
+  return (
+    <aside className={styles['customer-links']}>
+      {/* <div className={styles['container']}>{children}</div> */}
+      {children}
+    </aside>
+  );
+}
+
+CustomerLinks.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default CustomerLinks;
