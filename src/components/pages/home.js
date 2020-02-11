e768822084658fde418891282c00bc7c9086ff7d
krystek77 <krystian.wrona77@gmail.com>
2020-02-11 10:41:51 +0100

    Add news section to home page

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -3,6 +3,7 @@ import Layout from '../common/layout';
 import Hero from '../common/hero';
 import ServiceSection from '../home/service';
 import WhySection from '../home/why';
+import NewsSection from '../home/news';
 import Technology from '../home/technology';
 import bgImage from '../../assets/images/home/bg_hero_primus_dark-min.png';
 import { ActionButton } from '../common/buttonLink';
@@ -20,11 +21,12 @@ function HomePage() {
           partnerships. Holisticly deliver prospective web-readiness rather than
           integrated quality vectors. Authoritatively parallel task low-risk
           high-yield e-tailers for real-time niche.'>
-        <ActionButton path="/produkty" label="Poznaj produkty" />
+        <ActionButton path='/produkty' label='Poznaj produkty' />
       </Hero>
       <Technology />
       <WhySection />
       <ServiceSection />
+      <NewsSection />
     </Layout>
   );
 }
