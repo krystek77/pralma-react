46b5585fd6dd6c56da8b8d21469f5619e7acfe57
krystek77 <krystian.wrona77@gmail.com>
2020-02-13 10:27:18 +0100

    Add title props to picture component

--- src/components/common/picture/picture.js
+++ src/components/common/picture/picture.js
@@ -3,21 +3,28 @@ import PropTypes from 'prop-types';
 import styles from './picture.module.scss';
 import defaultImage from '../../../assets/images/default_460_460.png';
 
-function Picture({ src, alt, width }) {
+function Picture({ src, alt, title, width }) {
   return (
     <div className={styles['image-wrapper']} style={{ maxWidth: `${width}px` }}>
-      <img className={styles['image-wrapper__image']} src={src} alt={alt} />
+      <img
+        className={styles['image-wrapper__image']}
+        src={src}
+        title={title}
+        alt={alt}
+      />
     </div>
   );
 }
 Picture.propTypes = {
   src: PropTypes.string,
+  title: PropTypes.string,
   alt: PropTypes.string,
   width: PropTypes.number,
 };
 Picture.defaultProps = {
   src: defaultImage,
   alt: 'Default description of image',
+  title: 'Default title of image',
   width: 320,
 };
 
